8f52135dc953a71345b6831d846d0ebe887fbd7f
SriwanKm <kcqkoo@gmail.com>
2021-05-04 14:28:09 -0700

    moving between screens done

--- Components/About.js
+++ Components/About.js
@@ -3,10 +3,14 @@ import {StyleSheet, Text, View} from 'react-native';
 import {globalStyles} from "../styles/global";
 
 
-export default function About() {
+export default function About({navigation}) {
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>About page</Text>
+            <Text
+                style={globalStyles.titleText}
+                onPress={() => navigation.navigate('Home')}>
+                About page
+            </Text>
         </View>
 
     )
--- Components/Main.js
+++ Components/Main.js
@@ -3,10 +3,14 @@ import {StyleSheet, Text, View} from 'react-native';
 import {globalStyles} from "../styles/global";
 
 
-export default function Main() {
+export default function Main({navigation}) {
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Kookai's Review App</Text>
+            <Text
+                onPress={() => navigation.navigate('About')}
+                style={globalStyles.titleText}>
+                Kookai's Review App
+            </Text>
         </View>
 
     )
